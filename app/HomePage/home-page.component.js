97113a0203c7b3d6f51ebf7d84fb4ed26bd05ce9
Kademsky Alexander <kademskyi.alexander@gmail.com>
2024-05-12 22:05:01 +0300

    debug

--- app/HomePage/home-page.component.js
+++ app/HomePage/home-page.component.js
@@ -1,6 +1,7 @@
 const main = document.getElementById('main');
 if (main) {
     main.addEventListener('click', () => {
-        alert('Click on: ', this);
+        alert('Clicked!');
+        console.log(main);
     });
 }
