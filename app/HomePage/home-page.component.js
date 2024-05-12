d234a76f6a58dc3f1baf03105ead1c5b65e9b037
Kademsky Alexander <kademskyi.alexander@gmail.com>
2024-05-12 22:30:41 +0300

    debug

--- app/HomePage/home-page.component.js
+++ app/HomePage/home-page.component.js
@@ -1,5 +1,7 @@
 function setScope(scope) {
     document = scope;
+    console.log(scope);
+    console.log(document);
 }
 
 const main = document.getElementById('main');
@@ -7,5 +9,7 @@ if (main) {
     main.addEventListener('click', () => {
         alert('Clicked!');
         console.log(main);
+
+        console.log('2334');
     });
 }
