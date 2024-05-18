eade986b1c1f5ca826bc33b679874c0dcc4fc80e
Kademsky Alexander <kademskyi.alexander@gmail.com>
2024-05-19 01:18:07 +0300

    f

--- app/GuideMore/guide-more.component.js
+++ app/GuideMore/guide-more.component.js
@@ -1,6 +1,8 @@
 await HttpClient.getJson('database/classes.json').then((data) => {
     if (data) {
+        console.log(data);
         const _class = data.find((c) => c.id === window.routerParams.id);
+        console.log(_class);
         if (_class) {
             const guide = _class.guides.find((c) => c.id === window.routerParams.guideId);
 
