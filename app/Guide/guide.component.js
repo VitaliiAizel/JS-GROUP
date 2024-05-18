c20f48878b7aaebc291853b98950332f0d596f25
Kademsky Alexander <kademskyi.alexander@gmail.com>
2024-05-18 22:12:24 +0300

    f

--- app/Guide/guide.component.js
+++ app/Guide/guide.component.js
@@ -1,6 +1,6 @@
 const data = await HttpClient.getJson('database/classes.json');
 
 if (data) {
-    ComponentWorker.ngFor("classes", data);
-    ComponentWorker.ngFor("guides", data.guides);
+    ComponentWorker.ngFor(document, "classes", data);
+    ComponentWorker.ngFor(document, "guides", data.guides);
 }
