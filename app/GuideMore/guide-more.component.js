21e7d9c451336b7cf7764a991356cdd9036ed17f
Kademsky Alexander <kademskyi.alexander@gmail.com>
2024-05-19 15:30:30 +0300

    feat: added documentation

--- app/GuideMore/guide-more.component.js
+++ app/GuideMore/guide-more.component.js
@@ -1,23 +1,10 @@
 await HttpClient.getJson('database/classes.json').then((data) => {
-    if (data) {
-        console.log(data);
-        const _class = data.find((c) => c.id == window.routerParams.id);
-        if (_class) {
-            const guide = _class.guides.find((c) => c.id == window.routerParams.guideId);
-
-            console.log(guide);
-        }
-    }
+    const _class = data.find((c) => c.id == window.routerParams.id);
+    const guide = _class.guides.find((c) => c.id == window.routerParams.guideId);
 
     ComponentWorker.interpolate(
         document,
-        "id",
-        window.routerParams.id,
-    );
-
-    ComponentWorker.interpolate(
-        document,
-        "guideId",
-        window.routerParams.guideId,
+        ["id", "guideId"],
+        [guide.title, guide.id],
     );
 });
--- script.js
+++ script.js
@@ -1,37 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => onInit());
+document.addEventListener("DOMContentLoaded", onInit);
 
-const AvailableRoutes = [
-    {
-        route: 'home',
-        component: 'app-home',
-    },
-    {
-        route: 'guide',
-        component: 'app-guide',
-    },
-    {
-        route: 'guide/more',
-        component: 'app-guide-more',
-    },
-    {
-        route: 'game',
-        component: 'app-game',
-    },
-    {
-        route: 'faq',
-        component: 'app-faq',
-    }
+const components = [
+    { name: 'app-home', path: '/app/Home', component: 'home.component' },
+    { name: 'app-guide', path: '/app/Guide', component: 'guide.component' },
+    { name: 'app-guide-more', path: '/app/GuideMore', component: 'guide-more.component' },
+    { name: 'app-game', path: '/app/Game', component: 'game.component' },
+    { name: 'app-faq', path: '/app/Faq', component: 'faq.component' }
+];
+
+const availableRoutes = [
+    { route: 'home', component: 'app-home' },
+    { route: 'guide', component: 'app-guide' },
+    { route: 'guide/more', component: 'app-guide-more' },
+    { route: 'game', component: 'app-game' },
+    { route: 'faq', component: 'app-faq' }
 ];
 
 function onInit() {
     // Components
-    ComponentCreator.register('app-home', '/app/Home', 'home.component');
-    ComponentCreator.register('app-guide', '/app/Guide', 'guide.component');
-    ComponentCreator.register('app-guide-more', '/app/GuideMore', 'guide-more.component');
-    ComponentCreator.register('app-game', '/app/Game', 'game.component');
-    ComponentCreator.register('app-faq', '/app/Faq', 'faq.component');
+    components.forEach(({ name, path, component }) => {
+        ComponentCreator.register(name, path, component);
+    });
 
     // Router
-    window.AvailableRoutes = AvailableRoutes;
+    window.availableRoutes = availableRoutes;
     Router.loadComponent('home');
 }
