316e9bfee8278a18782d91b2654d75ccd8f41e04
Kademsky Alexander <kademskyi.alexander@gmail.com>
2024-05-19 20:17:04 +0300

    feat: added search

--- app/Guide/guide.component.js
+++ app/Guide/guide.component.js
@@ -6,3 +6,18 @@ await HttpClient.getJson('database/classes.json').then((data) => {
         [{ name: "guides", field: "guides", child: [] }]
     );
 });
+
+const search = document.getElementById('search');
+if (search) {
+    search.addEventListener('keyup', (e) => {
+        const value = e.target.value.toLowerCase();
+        document.querySelectorAll('[groupName]').forEach((el) => {
+            if (el.getAttribute('groupName').toLowerCase() == value) {
+                el.style.display = 'block';
+            }
+            else {
+                el.style.display = 'none';
+            }
+        });
+    });
+}
